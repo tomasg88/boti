8a39823dd3b043897cae81a16568c254b3943b3c
Tomas Gonzalez <AR00123160@latam1.prosegur.local>
2017-09-26 13:52:25 -0300

    ..

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,40 @@
+
 $(document).ready(function() {
   var SCREEN_BREAKPOINT = 992;
   var VIEWPORT = $(window).width();
   var IS_PORTRAIT = window.innerHeight > window.innerWidth;
+  var IS_MOBILE = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i);
   var IMG_PATH = 'img/'
   var LANDSCAPE_IMG = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
   var PORTRAIT_IMG = ['fondo 1 vertical.jpg', 'fondo 2 vertical.jpg', 'fondo 3 vertical.jpg', 'fondo 4 vertical.jpg'];
-  adaptLayout();
 
+  adaptLayout();
   $(window).resize(function (event) {
     VIEWPORT = $(window).width();
     IS_PORTRAIT = $(window).innerHeight() > $(window).innerWidth();
+    IS_MOBILE = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i);
 
     adaptLayout();
   });
 
   function adaptLayout() {
     // Mobile and Tablets
-    if (VIEWPORT < SCREEN_BREAKPOINT) {
+    if (VIEWPORT < SCREEN_BREAKPOINT && IS_MOBILE) {
       $(".top").removeClass("border-top");
       $(".left").removeClass("border-left");
+      $(".icon").addClass("extra-left");
     } else {
       // Web
       $(".top").addClass("border-top");
       $(".left").addClass("border-left");
+      $(".icon").removeClass("extra-left");
     }
-    console.log(IS_PORTRAIT);
     if (IS_PORTRAIT) {
       $('.slide').each(function(i, img) {
-        console.log('imagen',IMG_PATH + PORTRAIT_IMG[i]);
         $(img).attr('src', IMG_PATH + PORTRAIT_IMG[i]);
       });
     } else {
       $('.slide').each(function(i, img) {
-        console.log('imagen',IMG_PATH + LANDSCAPE_IMG[i]);
         $(img).attr('src', IMG_PATH + LANDSCAPE_IMG[i]);
       });
     }
